a3426a643bd1c81faa533712acef523b54d06395
kbolembach <konrad.bolembach@gmail.com>
2023-12-25 11:12:48 +0100

    implemented getting channels from folder, forwarding messages to target channel

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+from pyrogram import filters, idle, Client
+from pyrogram.raw import functions
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Client("my_account")
+target_channel = "jekoagregram"
+subscribed_dialogs = []
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+async def main():
+    async with app:
+        dialogs = await app.invoke(
+            functions.messages.GetDialogFilters()
+        )
+        dialogs = [d for d in dialogs if hasattr(d, "title")]
+        dialogs_folder = [d for d in dialogs if d.title == "Memes"][0]
+        global subscribed_dialogs
+        subscribed_dialogs = [int("-100" + str(d.channel_id)) for d in dialogs_folder.include_peers]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        @app.on_message(filters.chat(subscribed_dialogs))
+        async def fetch_feed(client, message):
+            await message.forward(target_channel)
+
+        await idle()
+
+
+app.run(main())
